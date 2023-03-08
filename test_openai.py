db169fbec9a24c3890ebde3b9eec2f9f40008a21
Andrea Spinazzola <50515354+spina95@users.noreply.github.com>
2023-03-09 00:49:37 +0100

    fixed query

--- test_openai.py
+++ test_openai.py
@@ -2,17 +2,57 @@ import openai
 import os
 import re 
 import spotipy
+import json
 from spotipy.oauth2 import SpotifyClientCredentials
 
 openai.api_key = "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"
+
+premessage = "answer this question in a json dictionary with title and artist: \n## "
+message = "Suggest me 10 soundtracks from the movies of Steven Spielberg released before 2005"
 r = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
       messages=[
-        {"role": "user", "content": "suggest me 15 songs about horses or dogs"}],
-    max_tokens=193,
+        {"role": "user", "content": premessage + message}],
     temperature=0,
 )
 data = r["choices"][0]["message"]['content']
+
+json_data = json.loads(data)
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="3568d9f9b3544af98e31131a9fcb02dd",
+                                                           client_secret="1a4d1e3af6274dfa89fab72604a65f86"))
+
+out = {}
+found_field = None
+for field in json_data:
+    if isinstance(json_data[field], list):
+        found_field = field
+
+for i, x in enumerate(json_data[found_field]):
+    title = x['title']
+    author = x['artist']
+    search_text = title + ' ' + author
+    result = sp.search(q='track:' + search_text, type='track', limit=1)
+    if result:
+        track = result['tracks']['items'][0]
+        id = track['id']
+        cover = track['album']['images'][0]['url']
+        title = track['name']
+        spotify_uri = track['uri']
+        preview_url = track['preview_url']
+        artists = ', '.join([x['name'] for x in track['artists']])
+        out[i] = {
+            "id": id,
+            "title": title,
+            "artists": artists,
+            "text": search_text,
+            "cover": cover,
+            "spotify_uri": spotify_uri,
+            "preview_url": preview_url
+        }
+    print(result)
+pass
+'''
 data = data.strip()
 split = re.split('\d\.', data)
 out = {}
@@ -42,3 +82,4 @@ for i, x in enumerate(split[1:]):
             "preview_url": preview_url
         }
     print(result)
+'''
