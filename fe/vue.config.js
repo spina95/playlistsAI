37a01f824ac91776245f188e594e08b8388930dc
Andrea Spinazzola <50515354+spina95@users.noreply.github.com>
2023-04-01 17:27:18 +0200

    test

--- fe/vue.config.js
+++ fe/vue.config.js
@@ -8,5 +8,25 @@ module.exports = defineConfig({
   },
   publicPath: process.env.BASE_URL,
     
-  assetsDir: process.env.BASE_URL
+  assetsDir: process.env.BASE_URL,
+  configureWebpack:{
+    mode: 'development',
+    devtool: false,
+    optimization: {
+      splitChunks: {
+        
+           
+        chunks: 'all',
+        minSize: 15000,
+        maxSize: 250000,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+        
+      },
+    
+     
+    },
+
+  }
 })
