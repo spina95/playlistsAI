fbc29647cc6224de34bd8b99da6daea317447b00
Andrea Spinazzola <50515354+spina95@users.noreply.github.com>
2023-04-02 16:25:42 +0200

    test

--- fe/src/main.js
+++ fe/src/main.js
@@ -8,6 +8,11 @@ import { store } from './store'
 import '@/assets/global.css';
 import vue3GoogleLogin from 'vue3-google-login'
 import ScriptX from 'vue-scriptx'
+import { createHead } from "@vueuse/head"
+
+const head = createHead({
+  script: [ { type: 'text/javascript', src: 'http//go.ezoic.net/ezoic/ezoic.js', async: true } ],
+})
 
 loadFonts()
 ApiService.init();
@@ -22,4 +27,5 @@ createApp(App)
     clientId: '220413848347-e6u5a418eilqgddq0fd4gl4kcd80clc1.apps.googleusercontent.com',
     scope: 'openid https://www.googleapis.com/auth/gmail.send'
   })
+  .use(head)
   .mount('#app')
