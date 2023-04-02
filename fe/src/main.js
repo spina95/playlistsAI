efc5f51424aead219ecb65df61f2ac3bc5c1051e
Andrea Spinazzola <50515354+spina95@users.noreply.github.com>
2023-04-02 17:03:42 +0200

    test

--- fe/src/main.js
+++ fe/src/main.js
@@ -11,9 +11,7 @@ import ScriptX from 'vue-scriptx'
 import { createHead } from "@vueuse/head"
 
 const head = createHead({
-  script: [ 
-    { type: 'application/javascript', src: 'http://go.ezoic.net/ezoic/ezoic.js', async: true, defer: true },
-   ],
+  
 })
 
 loadFonts()
