5f4c36a7c438e6f4b79b9d7e3ada26e97012ee09
Andrea Spinazzola <50515354+spina95@users.noreply.github.com>
2023-04-12 22:44:07 +0200

    fix

--- vite-fe/src/App.vue
+++ vite-fe/src/App.vue
@@ -1,7 +1,37 @@
 <template>
-  <router-view />
+  <v-app>
+    <Toolbar />
+    <v-main>
+        <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+import Toolbar from './components/Toolbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Toolbar
+  },
+  data: () => ({
+    //
+  }),
+  setup() {
+      /*
+      const googleSignInScript = document.createElement('script');
+      googleSignInScript.setAttribute('ezoicId', '444400')
+      googleSignInScript.setAttribute(
+        'src',
+        'http://go.ezoic.net/ezoic/ezoic.js'
+      );
+      googleSignInScript.setAttribute('async', 'true');
+      googleSignInScript.setAttribute('defer', 'true');
+      document.head.appendChild(googleSignInScript);
+      */
+      
+    },
+}
+
 </script>
