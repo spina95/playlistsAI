5721b350f029d0f2e8b805c0aa39ecd683092357
Andrea Spinazzola <50515354+spina95@users.noreply.github.com>
2023-04-13 21:24:58 +0200

    test

--- vite-fe/src/components/SearchBar.vue
+++ vite-fe/src/components/SearchBar.vue
@@ -10,11 +10,6 @@
       </v-btn>
       -->
     </v-row>
-
-    <!-- Ezoic - under_page_title - under_page_title -->
-    <div id="ezoic-pub-ad-placeholder-103" style="width: 100%; height: 50px;"> </div>
-    
-    <!-- End Ezoic - under_page_title - under_page_title -->
   
     <h3 class="pa-8" v-if="error" color="primary">An error has occured, please try again</h3>
     
